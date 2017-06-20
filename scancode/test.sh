f384b8e17eab7708f136488ff39d578a449dd962
Matt Rutkowski <mrutkows@us.ibm.com>
2017-06-20 15:12:43 -0500

    Add travis for all utilities in in subdirs.

--- scancode/test.sh
+++ scancode/test.sh
@@ -3,13 +3,17 @@ set -x #echo on
 
 SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 
+ls-al
+
 cd $HOMEDIR
 
+ls-al
+
 # Test help
-python ./$HOMEDIR/scanCode.py . -h
+python ($HOMEDIR)/scanCode.py . -h
 
 # Test basic usage
-python scancode/scanCode.py . --config travis.cfg
+python ./scancode/scanCode.py . --config travis.cfg
 
 # Test verbose usage
 python ./scanCode.py . --config travis.cfg -v
